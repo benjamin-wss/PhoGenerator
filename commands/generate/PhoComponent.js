8dabac5bc0974c4fe54d2238867a80693358d950
Benjamin Wong <benjamin.wss@gmail.com>
2019-06-28 23:26:30 +0800

    Added code.

--- commands/generate/PhoComponent.js
+++ commands/generate/PhoComponent.js
@@ -1,37 +1,43 @@
 /**
  * This is an example Ignite plugin generator. You can run it when it's installed to
  * your project by doing `ignite generate Phogenerator foo`.
- * 
+ *
  * You can rename this command to anything you'd like, or add others.
- * 
+ *
  * For more information on plugins, check out https://github.com/infinitered/gluegun/blob/master/docs/plugins.md.
  */
 
 module.exports = {
   description: "Example Phogenerator generator",
-  run: async function (toolbox) {
+  run: async function(toolbox) {
     // Learn more about toolbox: https://infinitered.github.io/gluegun/#/toolbox-api.md
-    const { parameters, strings, print, ignite } = toolbox
-    const { pascalCase, isBlank } = strings
+    const { parameters, strings, print, ignite } = toolbox;
+    const { pascalCase, isBlank } = strings;
 
     // validation
     if (isBlank(parameters.first)) {
-      print.info(`ignite generate Phogenerator <name>\n`)
-      print.info('A name is required.')
-      return
+      print.info(`ignite generate PhoComponent <name>\n`);
+      print.info("A name is required.");
+      return;
     }
 
-    const name = pascalCase(parameters.first)
-    const props = { name }
+    const name = pascalCase(parameters.first);
+    const props = { name };
 
     // Copies the `Phogenerator.js.ejs` in your plugin's templates folder
     // into App/Things/${name}.js.
-    const jobs = [{
-      template: 'Phogenerator.js.ejs',
-      target: `app/${name}.js`
-    }]
+    const jobs = [
+      {
+        template: "PhoComponent.js.ejs",
+        target: `app/components/${name}/index.js`
+      },
+      {
+        template: "PhoComponentStyle.js.ejs",
+        target: `app/components/${name}/style.js`
+      }
+    ];
 
     // make the templates and pass in props with the third argument here
-    await ignite.copyBatch(toolbox, jobs, props)
+    await ignite.copyBatch(toolbox, jobs, props);
   }
-}
+};
