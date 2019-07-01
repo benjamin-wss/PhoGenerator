ddc21534a0230eef9ea16ddddc5420aeac710458
Benjamin Wong <benjamin.wss@gmail.com>
2019-07-01 18:19:26 +0800

    Added saga and redux commands.

---
+++ commands/generate/PhoRedux.js
@@ -0,0 +1,24 @@
+  
+module.exports = {
+    description: ' Generates a action/creator/reducer set for Redux.',
+    run: async function(toolbox) {
+      // grab some features
+      const { parameters, ignite, strings, print } = toolbox
+      const { isBlank, pascalCase } = strings
+      const config = ignite.loadIgniteConfig();
+  
+      // validation
+      if (isBlank(parameters.first)) {
+        print.info(`${toolbox.runtime.brand} generate redux <name>\n`)
+        print.info('A name is required.')
+        return
+      }
+  
+      const name = pascalCase(parameters.first)
+      const props = { name }
+  
+      const jobs = [{ template: `pho.redux.ejs`, target: `App/Containers/${name}/redux.js` }]
+  
+      await ignite.copyBatch(toolbox, jobs, props)
+    }
+  }
